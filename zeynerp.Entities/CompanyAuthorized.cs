81cd649110095bb44d4ce9cb0b512e90b3b2ab26
User <User@DESKTOP-T7SEF7T>
2020-12-15 14:21:50 +0300

    cari detay sayfası

---
+++ zeynerp.Entities/CompanyAuthorized.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zeynerp.Entities
+{
+   public class CompanyAuthorized
+    {
+        public int Id { get; set; }
+        public string NameSurname { get; set; }
+
+        public string Phone { get; set; }
+        public string MobilePhone  { get; set; }
+
+        public string Fax { get; set; }
+
+        public string Email { get; set; }
+
+        public IsActive IsActive { get; set; }
+        public int CompanyId { get; set; }
+        public Company Companies { get; set; }
+    }
+    public enum IsActive
+    {
+        Pasif,
+        Aktif
+    }
+}
+
+
---
+++ zeynerp.Entities/ViewModels/CompanyViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zeynerp.Entities.ViewModels
+{
+   public class CompanyViewModel
+    {
+        public Company Companies { get; set; }
+        public IEnumerable<CompanyAuthorized> CompanyAuthorizeds { get; set; }
+    }
+}
