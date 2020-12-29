a0b6d5074dcadbb07f6e688ebc71bcff99cdc495
Sefa Türk <sefatrk@outlook.com.tr>
2020-12-29 16:50:38 +0300

    .

--- zeynerp/Controllers/HomeController.cs
+++ zeynerp/Controllers/HomeController.cs
@@ -190,6 +190,31 @@ namespace zeynerp.Controllers
                 return RedirectToAction("SignIn");
             }
 
+        [Authorize]
+        [HttpGet]
+        public ActionResult CompanyAdd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CompanyAdd(Company companyModel)
+        {
+            Employee employee = Session["employee"] as Employee;
+
+            int Id = companyProcess.CompanyAdd(employee, companyModel);
+            return RedirectToAction("CompanyDetail", new { Id });
+        }
+
+        public ActionResult CompanyList()
+        {
+            Employee employee = Session["employee"] as Employee;
+
+            if (employee == null)
+            {
+                return RedirectToAction("SignIn");
+            }
+
             List<Company> companies = companyProcess.GetCompanyList(employee);
             return View(companies);
         }
@@ -209,6 +234,14 @@ namespace zeynerp.Controllers
             companyviewModel.CompanyAuthorizeds = companyProcess.GetCompanyAuthorizedList(employee, id);
             List<CompanyAuthorized> authorized = companyProcess.GetCompanyAuthorizedList(employee, id);
             return View(companyviewModel);
+        }
+
+        [HttpPost]
+        public ActionResult CompanyDetail(Company companyModel)
+        {
+            Employee employee = Session["employee"] as Employee;
+            Company comp = companyProcess.CompanyUpdate(employee, companyModel);
+            return View(comp);
         }
 
         [HttpPost]
